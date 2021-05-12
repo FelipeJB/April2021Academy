4bbfd231fdbe342cfdbdf15cacd0f83bd7344b17
juancarlos.ortiz <juancarlos.ortiz@CO-IT005101.globant.com>
2021-05-11 19:29:28 -0500

    Project Canvas

---
+++ src/Runner/Main.java
@@ -0,0 +1,14 @@
+package Runner;
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
